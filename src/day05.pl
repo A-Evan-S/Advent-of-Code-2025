14e618c72fe573cab1bda26a9e3c3c44cc862ac9
Evan S <aevanschor@gmail.com>
2025-12-04 22:14:46 -0800

    Minor refactoring

--- src/day05.pl
+++ src/day05.pl
@@ -1,16 +1,17 @@
 :- ['src/util.pl'].
 
-parse_range(RangeString, Range):-
-    split_string(RangeString, "-", "", S),
-    maplist(string_number, S, Range).
+parse_range(RangeString, [Start, End]):-
+    split_string(RangeString, "-", "", [A, B]),
+    number_string(Start, A),
+    number_string(End, B).
 
 in_ranges(Ranges, ID):-
     member([Start, End], Ranges),
     between(Start, End, ID).
 
 overlap([S1, E1], [S2, E2]):-
-    S1 =< S2, E1 >= S2;
-    S2 =< S1, E2 >= S1.
+    S1 =< E2,
+    S2 =< E1.
 
 merge_range_rangelist(Range, RangeList, Result):-
     partition(overlap(Range), RangeList, OverlappingRanges, OtherRanges),
@@ -20,7 +21,7 @@ merge_range_rangelist(Range, RangeList, Result):-
     Result = [[Start, End]|OtherRanges].
 
 range_size([A, B], RangeSize):-
-    RangeSize = B - A + 1.
+    RangeSize is B - A + 1.
 
 main:-
     Filename = 'inputs/day05.txt',
