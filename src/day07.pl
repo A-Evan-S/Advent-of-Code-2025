8a5282015a3bbccb8bdfcfd7dd5f58ab71a251a4
Evan S <aevanschor@gmail.com>
2025-12-06 23:15:56 -0800

    Factored out `split_beams` operation

--- src/day07.pl
+++ src/day07.pl
@@ -2,33 +2,34 @@
 
 main:-
     phrase_from_file(input(StartingColumn, Rows), 'inputs/day07.txt'),
-
     list_to_assoc([StartingColumn-1], Beams),
     foldl(process_line, Rows, (Beams, 0), (TotalBeams, Result1)),
     assoc_to_values(TotalBeams, BeamCounts),
     sum_list(BeamCounts, Result2),
-
-    write("Part 1: "), write(Result1), nl,
-    write("Part 2: "), write(Result2), nl,
-
+    format('Part 1: ~w~n', [Result1]),
+    format('Part 2: ~w~n', [Result2]),
     halt.
 
 process_line([], State, State).
 process_line([Splitter|Rest], (Beams, Splits), (ResBeams, Splits1)):-
-    get_assoc(Splitter, Beams, Count),
+    get_assoc(Splitter, Beams, _),
     process_line(Rest, (Beams, Splits), (RecBeams, RecSplits)),
-    succ(L, Splitter),
-    succ(Splitter, R),
-    (get_assoc(L, RecBeams, LCount) ; LCount = 0),
-    (get_assoc(R, RecBeams, RCount) ; RCount = 0),
-    del_assoc(Splitter, RecBeams, _, Beams1),
-    LCountNew is LCount + Count,
-    RCountNew is RCount + Count,
-    put_assoc(L, Beams1, LCountNew, Beams2),
-    put_assoc(R, Beams2, RCountNew, ResBeams),
+    split_beams(Splitter, RecBeams, ResBeams),
     succ(RecSplits, Splits1).
 process_line([_|Rest], State, ResultingState):-
     process_line(Rest, State, ResultingState).
+
+split_beams(Splitter, Beams, ResBeams):-
+    get_assoc(Splitter, Beams, Count),
+    succ(L, Splitter),
+    succ(Splitter, R),
+    (get_assoc(L, Beams, LCount) ; LCount = 0),
+    (get_assoc(R, Beams, RCount) ; RCount = 0),
+    del_assoc(Splitter, Beams, _, Beams1),
+    LCountNew is LCount + Count,
+    RCountNew is RCount + Count,
+    put_assoc(L, Beams1, LCountNew, Beams2),
+    put_assoc(R, Beams2, RCountNew, ResBeams).
 
 % Parsing
 input(StartingColumn, Rows) -->
